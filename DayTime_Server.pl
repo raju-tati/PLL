ec9c1cb7042be60894cbe87fe2d320d63039a7ca
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-02-25 12:26:58 +0530

    Create DayTime_Server.pl

---
+++ DayTime_Server.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+use EV;
+use AnyEvent;
+use AnyEvent::Socket;
+
+tcp_server undef, 8888, sub {
+   my ($fh, $host, $port) = @_;
+  
+   use DateTime;
+   my $dt = DateTime->now;
+   $dt->set_time_zone('Asia/Kolkata');
+        
+   my $year = $dt->year;
+   my $day = $dt->day;
+   my $month_abbr = $dt->month_abbr;
+   my $day_abbr = $dt->day_abbr;
+   my $hms = $dt->hms;
+
+   my $time = "$hms $day_abbr-$day-$month_abbr $year";      
+   syswrite $fh, "$time \015\012";
+};
+
+EV::loop();
