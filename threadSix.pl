46b4309c6f9c3e84a6321e63d7e5081619de963f
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-04-25 12:52:09 +0530

    Create threadSix.pl

---
+++ threadSix.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use experimentals;
+use threads;
+use Time::HiRes;
+
+sub main() {
+    foreach (0..100) {
+        Time::HiRes::sleep(0.01);
+        async {
+            say "running thread ", threads->self()->tid();
+        };
+    }
+}
+
+my $monitorThread = async {
+    while(1) {
+        foreach my $thread (threads->list(threads::joinable)) {
+            $thread->detach();
+        }
+        Time::HiRes::sleep(0.01);
+    }
+};
+main();
+while(1) {
+    my @threads = threads->list(threads::all);
+    if(scalar @threads == 1) {
+        $monitorThread->detach();
+        last;
+    } else {
+        Time::HiRes::sleep(0.01);
+    }
+}
+exit();
