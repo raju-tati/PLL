97d4224c82e8d1a0e84dbe92977d5e88535b3731
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-04-25 20:43:20 +0530

    Create threadEight.pl

---
+++ threadEight.pl
@@ -0,0 +1,55 @@
+use strict;
+use warnings;
+use experimentals;
+use threads;
+use threads::shared;
+use Time::HiRes;
+
+my %qq :shared;
+
+sub main() {
+    async {
+        {
+            lock(%qq);
+            $qq{"key"} = 1;
+        }
+    };
+    async {
+        {
+            lock(%qq);
+            my $key = $qq{"key"};
+            $qq{"key"} = $key + 1;
+        }
+    };
+    say "key is: ", $qq{"key"};
+    Time::HiRes::sleep(0.01);
+    say "key is: ", $qq{"key"};
+}
+
+my $signalThread = async {
+    use sigtrap 'handler' => \&signalHandler, qw(INT);
+};
+sub signalHandler($signalName) {
+    say "got an intterupt, print some info";
+    my @threads = threads->list(threads::all);
+    say "remaining threads: ", scalar @threads;
+}
+my $monitorThread = async {
+    while(1) {
+        foreach my $thread (threads->list(threads::joinable)) {
+            $thread->detach();
+        }
+        Time::HiRes::sleep(0.005);
+    }
+};
+main();
+while(1) {
+    my @threads = threads->list(threads::all);
+    if(scalar @threads == 1) {
+        $monitorThread->detach();
+        last;
+    } else {
+        Time::HiRes::sleep(0.01);
+    }
+}
+exit();
