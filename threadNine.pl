22ddf5d6ba9dd2efc93ee49ba209107c3ff9c533
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-05-23 21:41:04 +0530

    Create threadNine.pl

---
+++ threadNine.pl
@@ -0,0 +1,39 @@
+package Main;
+use Needs;
+
+sub main($class) {
+    async {
+        if( threads->self->tid() == 2 ) {
+           say "testing threads";
+        } else {}
+    };
+}
+
+1;
+
+use Needs;
+sub begin() {
+    my $mainObject = Main->new();
+    $mainObject->main();
+}
+
+my $monitorThread = async {
+    while(1) {
+        foreach my $thread (threads->list(threads::joinable)) {
+            $thread->detach();
+        }
+        Time::HiRes::sleep(0.005);
+    }
+};
+
+begin();
+
+while(1) {
+    my @threads = threads->list(threads::all);
+    if(scalar @threads == 1) {
+        $monitorThread->detach();
+        last;
+    } else {
+        Time::HiRes::sleep(0.001);
+    }
+}
