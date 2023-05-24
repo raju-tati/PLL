d31a65c3fd92a9b3165aab06f52781cfee910e73
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-05-24 21:24:07 +0530

    Update threadNine.pl

--- threadNine.pl
+++ threadNine.pl
@@ -1,16 +1,21 @@
-package Main;
-use Basics;
+use strict;use warnings;use utf8;use Time::HiRes;
+use threads;use threads::shared;use Object::Pad;use experimentals;
 
-sub main($this) {
-    async {
-        say "testing threads";
-    };
+class Main {
+    use strict;use warnings;use utf8;use threads;
+    use threads::shared;use experimentals;use Time::HiRes;
+
+    field $upTime :param;
+
+    method main() {
+        async {
+            say "upTime: ", $upTime;
+            say "from thread: ", threads->self->tid();
+        };
+    }
 }
 
-1;
-
-use Basics;
-sub begin(){my $mainObject = Main->new();$mainObject->main();}
+sub begin(){my $mainObject=Main->new(upTime=>time());$mainObject->main();}
 my $monitorThread = async { while(1) {
 foreach my $thread (threads->list(threads::joinable)) {
 $thread->detach(); } Time::HiRes::sleep(0.005); }};
